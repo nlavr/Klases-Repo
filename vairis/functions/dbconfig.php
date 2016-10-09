379cf88b31c38a90a711ccf239bf82f826d51fa2
Vairis <vairiks123@gmail.com>
2016-10-09 18:53:35 +0300

    Mazas izmainas koda

--- vairis/functions/dbconfig.php
+++ vairis/functions/dbconfig.php
@@ -4,4 +4,8 @@ $username = 'root';
 $password = '';
 $database = 'myshop';
 $dbconfig = mysqli_connect($host,$username,$password,$database);
+
+    if ($dbconfig->connect_errno) {
+        die("ERROR : -> " . $dbconfig->connect_error);
+    }
 ?>
