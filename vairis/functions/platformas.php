4af98cb0d2480b84cd2a32ec652ab22487e188cf
Vairis <vairiks123@gmail.com>
2016-10-11 22:01:13 +0300

    pohuj

--- vairis/functions/platformas.php
+++ vairis/functions/platformas.php
@@ -17,15 +17,15 @@ function getMenuTree()
     // Dabū no db ārā visas platformas un saglabā $items masīvā
     while($platform = mysqli_fetch_array($platforms)) {
         $items[$platform['ID']] = [
-            'name' => $platform['Name'],// Katrai platformai ir name
+            'name' => $platform['name'],// Katrai platformai ir name
             'categories' => [] // Katrai platformai var būt piesaistītas kategorijas (tukšums pēc noklusējuma)
         ];
     }
     // Dabū no db ārā visas kategorijas un saglabā $items masīvā pie attiecīgās platformas
     while($category = mysqli_fetch_array($categories))   {
         // Viņš saglabā kategorijas nosaukumu iekš attiecīgās platformas categories saraksta beigās
-        $items[$category['Parent']]['categories'][$category['ID']] = [
-            'name' => $category['Name']
+        $items[$category['parentID']]['categories'][$category['ID']] = [
+            'name' => $category['name']
         ];
     }
     return $items;
